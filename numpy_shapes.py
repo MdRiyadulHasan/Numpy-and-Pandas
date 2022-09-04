7ffca29a36a12d651d1519fc5ab8ea24fa91aadc
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-09-04 12:11:44 +0600

    Create numpy_shapes.py

---
+++ numpy_shapes.py
@@ -0,0 +1,11 @@
+from itertools import tee
+import numpy as np
+temperatures=np.array([
+    29.3, 42.1, 18.8, 16.1, 38.0, 12.5, 12.6, 49.9, 38.6, 31.3, 9.2, 22.2
+])
+#reshape numpy array
+temperatures=temperatures.reshape(2,2,3)
+print(temperatures)
+# Swap axes of numpy array
+temperatures=np.swapaxes(temperatures,1,2)
+print(temperatures)
