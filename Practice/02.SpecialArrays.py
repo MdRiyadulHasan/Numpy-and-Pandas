e57d577f87fa24e9e5555a60e6a3cdc8f995410b
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2025-03-26 13:24:24 +0600

    Numpy

---
+++ Practice/02.SpecialArrays.py
@@ -0,0 +1,9 @@
+import numpy as np
+zero_matrix=np.zeros((3,3))
+print(zero_matrix)
+one_matrix = np.ones((4,3))
+print(one_matrix)
+full_matrix = np.full((2,2), 9)
+print(full_matrix)
+identity_matrix = np.eye(3)
+print(identity_matrix)
