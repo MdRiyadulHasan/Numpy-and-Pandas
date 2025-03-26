b498df78fc102a25363812aa697bf35dec462469
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2025-03-26 14:18:15 +0600

    Numpy...

---
+++ Practice/05.MathOperations.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)  # [5, 7, 9]
+print(a * b)  # [4, 10, 18]
+print(a / 2)  # [0.5, 1.0, 1.5]
+print(a+[2])
+print(b-[3])
